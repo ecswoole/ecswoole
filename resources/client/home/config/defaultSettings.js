f0cc8c1021aac7e5802e6dd7c4e9e4cc7048160f
wanganlin <inbox@wanganlin.com>
2020-09-11 13:50:04 +0800

    add clients

---
+++ resources/client/home/config/defaultSettings.js
@@ -0,0 +1,18 @@
+const proSettings = {
+  navTheme: 'light',
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Ant Design Pro',
+  pwa: false,
+  iconfontUrl: '',
+  splitMenus: false,
+  footerRender: false
+};
+export default proSettings;
